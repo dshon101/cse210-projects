db7cea0127e0ce92c383a40aa0f97912c2764362
Demetrious Shoniwa <84961911+dshon101@users.noreply.github.com>
2024-10-02 04:36:14 +0200

    Added all the files to a different folder and added more code to them.

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -5,9 +5,10 @@ public class Entry
     public string _date;
     public string _promptText;
     public string _entryText;
-
     public void Display()
     {
-        
+        Console.WriteLine(_date);
+        Console.WriteLine(_promptText);
+        Console.WriteLine(_entryText);
     }
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
+        Entry userEntry = new Entry();
+        // DateTime dt = DateTime.Now;
+        userEntry._date = 
+        userEntry._promptText = "What did you do today.";
+        userEntry._entryText = Console.ReadLine();
+
+
         Console.WriteLine("Hello Develop02 World!");
+        userEntry.Display();
     }
 }

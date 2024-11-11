8e00c1bccd449ffff6489d15b1aefb89de736688
Demetrious Shoniwa <84961911+dshon101@users.noreply.github.com>
2024-11-11 04:46:12 +0200

    Changed the files in the csharp-prep folder and added some code in them to get a better understanding of csharp.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,61 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage: ");
+        string userInput = Console.ReadLine();
+
+        int gradePercentage = int.Parse(userInput);
+        
+        string letter = "F";
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >=80 && gradePercentage < 90)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >=70 && gradePercentage < 80)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >=60 && gradePercentage < 70)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        int sign = gradePercentage % 10;
+        string gradeSign = " ";
+        
+        if (!(gradePercentage > 93))
+        {
+            if (sign >= 7)
+            {
+                gradeSign = "+";
+            }
+            else if (sign < 3)
+            {
+                gradeSign = "-";
+            }
+            else
+            {
+                gradeSign = " ";
+            }
+        }
+        
+        Console.WriteLine($"{letter}{gradeSign}");
+        
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations you passed the course");
+        }
+        else 
+        {
+            Console.WriteLine("Sorry you didn\'t pass the course. Please try again next semester.");
+        }
     }
 }

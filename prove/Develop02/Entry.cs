aade7bf590e0cbdd501c3cdb035907865b84e815
Demetrious Shoniwa <84961911+dshon101@users.noreply.github.com>
2024-11-19 16:46:33 +0200

    The draft for my Journal program

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,13 @@
+using System;
+using System.IO;
+
+public class Entry
+{
+    public string _date;
+    public string _promptText;
+    public string _entryText;
+    public void Display()
+    {
+
+    }
+}
---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Journal
+{
+    public List<Entry> _entriers;
+
+    public void AddEntry(Entry newEntry)
+    {
+
+    }
+
+    public void DisplayAll()
+    {
+
+    }
+    public void SaveToFile(string file)
+    {
+
+    }
+    public void LoadFromFile(string file)
+    {
+
+    }
+}

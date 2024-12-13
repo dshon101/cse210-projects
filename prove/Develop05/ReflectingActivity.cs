45efd152b0871ede6b0991ef483fc975455de15e
Demetrious Shoniwa <84961911+dshon101@users.noreply.github.com>
2024-12-13 19:43:31 +0200

    Created the first part of my code and it runs well.

---
+++ prove/Develop05/ReflectingActivity.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;  
+
+public class ReflectingActivity : Activity  
+{  
+    private List<string> _questions;  
+
+    public ReflectingActivity()  
+    {  
+    }  
+
+    public void Run()  
+    {  
+    }  
+
+    public string GetRandomPrompt()  
+    {  
+        return "";  
+    }  
+
+    public string GetRandomQuestion()  
+    {  
+        return "";  
+    }  
+
+    public void DisplayQuestions()  
+    {  
+    }  
+}  

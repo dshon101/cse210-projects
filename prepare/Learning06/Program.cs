b33f4689056153743377e5d224a478808f142e7f
Demetrious Shoniwa <84961911+dshon101@users.noreply.github.com>
2024-12-14 18:15:38 +0200

    Create an area program to find the area of different shapes using the color of those shapes. I used Polynorphism to override a methode in my code and it was a much better thing to do and to do it over and over again. I also used the aspect of Inheritance to not repeate a variable over and over againg.

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Drawing;
 
-class Program
+namespace area_of_shapes
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Learning06 World!");
+        static void Main(string[] args)
+        {
+            Square square = new Square("Red", 4);
+            Rectangle rectangle = new Rectangle("White", 5, 2);
+            Circle circle = new Circle("Blue", 2);
+            
+            List<Shape> shapes = new List<Shape>();
+            shapes.Add(square);
+            shapes.Add(rectangle);
+            shapes.Add(circle);
+
+            foreach (Shape shape in shapes)
+            {
+                string color = shape.GetColor();
+                double area = shape.GetArea();
+
+                Console.WriteLine($"The {color} shape has an area of {area}.");
+            }
+        }
     }
 }
